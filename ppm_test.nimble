f87431d89b0f62724ad2ee1c541e91db73be4a9b
Seiichi Ariga <seiichi.ariga@gmail.com>
2022-01-01 17:38:19 +0900

    First commit

---
+++ ppm_test.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Seiichi Ariga"
+description   = "Create PPM image fime."
+license       = "MIT"
+srcDir        = "src"
+bin           = @["ppm_test"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.0"
---
+++ src/ppm_test.nim
@@ -0,0 +1,23 @@
+# This is just an example to get you started. A typical binary package
+# uses this file as the main entry point of the application.
+
+import std/streams
+
+const nx = 200
+const ny = 100
+
+var strm = newFileStream("image.ppm", fmWrite)
+
+if not isNil(strm):
+  strm.writeLine("P3")
+  strm.writeLine(nx, " ", ny)
+  strm.writeLine("255")
+  for j in 1..ny:
+    for i in 1..nx:
+      let r = float(i)/float(nx)
+      let g = float(j)/float(ny)
+      let b = 0.5
+      let ir = int(r*255)
+      let ig = int(g*255)
+      let ib = int(b*255)
+      strm.writeLine(ir, " ", ig, " ", ib)
